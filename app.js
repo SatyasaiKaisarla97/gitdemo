7ca94b7f9ab026f841f5745e9a6b3146a6f38dd3
Satyasai <satyasai1997@gmail.com>
2023-11-09 23:10:29 +0530

    index.html

---
+++ app.js
@@ -0,0 +1,6 @@
+const k = document.getElementById("header-title")
+k.style.border = "thick solid black";
+
+const p = document.querySelector("h2")
+p.style.fontWeight = "bold"
+p.style.color = "green"
