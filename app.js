ece21ee75034806537c231b6c5f951a3b362a7fd
Satyasai <satyasai1997@gmail.com>
2023-11-10 12:21:10 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const k = document.getElementById("header-title")
+const k = document.getElementById("header-title");
 k.style.border = "thick solid black";
 
-const p = document.querySelector("h2")
-p.style.fontWeight = "bold"
-p.style.color = "green"
+const p = document.querySelector("h2");
+p.style.fontWeight = "bold";
+p.style.color = "green";
+
+const r = document.getElementsByClassName("list-group-item");
+r[2].style.backgroundColor = "green";
+for(i=0;i<=3;i++){
+    r[i].style.fontWeight = "bold";
+}
+
+
